29641778b59d820c4d5ca2825809810de2253644
Natallia Tserashchuk <tereshchuk.natasha@gmail.com>
2018-07-27 17:32:11 +0300

    Sample app for the react

---
+++ src/components/homePage.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function() {
+        return(
+            <div className="jumbotron">
+            <h1>Administration</h1></div>)
+    }
+})
---
+++ src/main.js
@@ -0,0 +1,8 @@
+$ = jQuery = require('jquery');
+
+var React = require('react');
+var homePage = require('./components/homePage');
+
+var App = console.log('Hello world');
+
+module.exports = App;
